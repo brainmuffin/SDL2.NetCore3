c87bac11f8ae146ed4e7ac10631e303688151acb
brainmuffin <bryonlape@gmail.com>
2020-11-17 20:44:49 -0500

    Updates for Timer

--- SDL2.NetCore3/SDL.cs
+++ SDL2.NetCore3/SDL.cs
@@ -14,38 +14,28 @@ namespace SDL2.NetCore3
         public const int SDL_INIT_NOPARACHUTE = 0x00100000;  /**< Don't catch fatal signals */
         public const int SDL_INIT_EVERYTHING = (
                         SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS |
-                        SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC | SDL_INIT_GAMECONTROLLER
-                    );
+                        SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC | SDL_INIT_GAMECONTROLLER);
 
-        private delegate int SDL_Init_UInt32_t(UInt32 flags);
-
-        private static SDL_Init_UInt32_t s_SDL_Init_UInt32_t = __LoadFunction<SDL_Init_UInt32_t>("SDL_Init");
-
-        public static int SDL_Init(UInt32 flags) => s_SDL_Init_UInt32_t(flags);
+        private delegate int SdlInitUInt32T(UInt32 flags);
+        private static readonly SdlInitUInt32T SSdlInitUInt32T = __LoadFunction<SdlInitUInt32T>("SDL_Init");
+        public static int SDL_Init(uint flags) => SSdlInitUInt32T(flags);
 
         private delegate int SDL_InitSubSystem_UInt32_t(UInt32 flags);
-
         private static SDL_InitSubSystem_UInt32_t s_SDL_InitSubSystem_UInt32_t = __LoadFunction<SDL_InitSubSystem_UInt32_t>("SDL_InitSubSystem");
-
         public static int SDL_InitSubSystem(UInt32 flags) => s_SDL_InitSubSystem_UInt32_t(flags);
 
         private delegate void SDL_QuitSubSystem_UInt32_t(UInt32 flags);
-
         private static SDL_QuitSubSystem_UInt32_t s_SDL_QuitSubSystem_UInt32_t = __LoadFunction<SDL_QuitSubSystem_UInt32_t>("SDL_QuitSubSystem");
-
         public static void SDL_QuitSubSystem(UInt32 flags) => s_SDL_QuitSubSystem_UInt32_t(flags);
 
         private delegate UInt32 SDL_WasInit_UInt32_t(UInt32 flags);
-
         private static SDL_WasInit_UInt32_t s_SDL_WasInit_UInt32_t = __LoadFunction<SDL_WasInit_UInt32_t>("SDL_WasInit");
-
         public static UInt32 SDL_WasInit(UInt32 flags) => s_SDL_WasInit_UInt32_t(flags);
 
-        private delegate void SDL_Quit__t();
-
-        private static SDL_Quit__t s_SDL_Quit__t = __LoadFunction<SDL_Quit__t>("SDL_Quit");
-
-        public static void SDL_Quit() => s_SDL_Quit__t();
+        private delegate void SdlQuitT();
+        private static readonly SdlQuitT SSdlQuitT = __LoadFunction<SdlQuitT>("SDL_Quit");
+        public static void SDL_Quit() => SSdlQuitT();
+        
         private static T __LoadFunction<T>(string name)
         {
             return Internal.Loader_SDL2.LoadFunction<T>(name);
